df718d0fa5e76b9afaa8d755ac8d34a5fc91ede7
Johny Rulan <johnyrulan@gmail.com>
2019-05-08 21:12:24 -0400

    Sets up project.

---
+++ Paylocity.Test/PaycheckServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Paylocity.Domain.Constants;
+using Paylocity.Domain.Interfaces;
+using Paylocity.Domain.Models;
+using Paylocity.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paylocity.Test
+{
+    [TestClass]
+    public class PaycheckServiceTests
+    {
+        private Mock<IEmployeeRepository> mockEmployeeRepository;
+        private IPaycheckService paycheckService;
+
+        private readonly string testEmployeeName = "Johny";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            paycheckService = new PaycheckService(mockEmployeeRepository.Object);            
+
+            mockEmployeeRepository
+                .Setup(_ => _.Get(It.IsAny<string>()))
+                .Returns(new Employee {
+                    Name = testEmployeeName,
+                    Dependents = new List<Person>()
+                });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PaycheckService_Get_Should_Return_Null_If_No_Employee_Found()
+        {
+            mockEmployeeRepository
+                .Setup(_ => _.Get(It.IsAny<string>()))
+                .Returns<Paycheck>(null);
+
+            var paycheck = paycheckService.Get(string.Empty);            
+        }
+
+        [TestMethod]        
+        public void PaycheckService_Get_Should_Return_Base_Employee_Pay()
+        {
+            var paycheck = paycheckService.Get(testEmployeeName);
+
+            Assert.AreEqual(EmployeeConstants.EmployeeBasePay, paycheck.BasePay);
+        }
+    }
+}
---
+++ Paylocity.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paylocity.Domain.Interfaces;
+using Paylocity.Domain.Models;
+
+namespace Paylocity.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+            :base()
+        {
+            this.employeeService = employeeService;
+        }
+
+        public IActionResult List()
+        {
+            var employees = employeeService.List();
+
+            return View(employees);
+        }
+
+        public IActionResult Get(string name)
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Add(Employee employee)
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(Employee employee)
+        {
+            return View();
+        }
+    }
+}
